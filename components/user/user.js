e59cab4ef0c098bf370688ca45c2a27fa263bab2
Kawaljit Singh <kawaljit.singh@leoburnett.com.au>
2019-08-30 15:45:23 +1000

    [css changes]

--- components/user/user.js
+++ components/user/user.js
@@ -1,17 +1,22 @@
 import './user.scss';
-
 const User = (props) => (
   <div className="git-user-card">
     <div className="git-userBio">
       <img src={props.userData.avatar_url} className="git-userAvatar"/>
       <h1 className="git-userName">{props.userData.name}</h1>
       <p className="git-userBioText">{props.userData.bio}</p>
-      <span className="git-userCompany">{props.userData.company}</span>
+      <p className="git-userCompany">{props.userData.company}</p>
+      <p className="git-userCompany">{props.userData.location}</p>
+      <span>{props.userData.followers}</span>
+      <span>{props.userData.following}</span>
+      <p>
+        <a href={props.userData.html_url}>Check on Github</a>
+      </p>
     </div>
     <div className="git-userStats">
-      <div className="git-userStatsCard">asdf</div>
-      <div className="git-userStatsCard">asdf</div>
-      <div className="git-userStatsCard">afsd</div>
+      <div className="git-userStatsCard"></div>
+      <div className="git-userStatsCard"></div>
+      <div className="git-userStatsCard"></div>
     </div>
   </div>
 );
