e0ba54fb962932d2d42091e751e7c6251ac3194b
Kawaljit Singh <kawaljit.singh@leoburnett.com.au>
2019-08-30 16:14:50 +1000

    change to function

--- components/user/user.js
+++ components/user/user.js
@@ -1,35 +1,38 @@
 import './user.scss';
-const User = (props) => (
-  <div className="git-user-card">
-    <div className="git-userBio">
-      <img src={props.userData.avatar_url} className="git-userAvatar"/>
-      <h1 className="git-userName">{props.userData.name}</h1>
-      <p className="git-userBioText">{props.userData.bio}</p>
-      <ul className="git-userVcard">
-        <li className="list">
-          <span>
-            {props.userData.company}
-          </span>
-        </li>
-        <li className="list">
-          <span>
-            {props.userData.location}
-          </span>
-        </li>
-        <li className="list">
-          <a href={props.userData.html_url}>Check on Github</a>
-        </li>
-      </ul>
-    </div>
-    <div className="git-userStats">
-      <div className="git-userStatsCard"></div>
-      <div className="git-userStatsCard">
-        <span>Followers on github{props.userData.followers}</span>
-        <span>Following on github: {props.userData.following}</span>
+export default function User(props) {
+  return (
+    <div className="git-user-card">
+      <div className="git-userBio">
+        <img
+          src={props.userData.avatar_url}
+          className="git-userAvatar"
+        />
+        <h1 className="git-userName">{props.userData.name}</h1>
+        <p className="git-userBioText">{props.userData.bio}</p>
+        <ul className="git-userVcard">
+          <li className="list">
+            <span>
+              {props.userData.company}
+            </span>
+          </li>
+          <li className="list">
+            <span>
+              {props.userData.location}
+            </span>
+          </li>
+          <li className="list">
+            <a href={props.userData.html_url}>Check on Github</a>
+          </li>
+        </ul>
       </div>
-      <div className="git-userStatsCard"></div>
+      <div className="git-userStats">
+        <div className="git-userStatsCard"></div>
+        <div className="git-userStatsCard">
+          <span>Followers on github{props.userData.followers}</span>
+          <span>Following on github: {props.userData.following}</span>
+        </div>
+        <div className="git-userStatsCard"></div>
+      </div>
     </div>
-  </div>
-);
-
-export default User;
+  )
+}
